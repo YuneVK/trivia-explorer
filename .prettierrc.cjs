c5d4637dfc258c4f61fe5fd3eaa14dd534dd23cc
Sonia Ruiz <yune.vk@gmail.com>
2024-03-05 23:18:19 +0100

    ➕ 🎨 Install Prettier, add config, and format script

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
