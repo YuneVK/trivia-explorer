fd8023039eede85f505b0d448305ac7cb6584fd7
Sonia Ruiz <yune.vk@gmail.com>
2024-03-07 00:14:35 +0100

    💚 Fix Vite base config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import postcssNested from 'postcss-nested'
 
-const mode = import.meta.env.MODE
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: mode === 'production' ? '/trivia-explorer/' : '/',
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [postcssNested],
+export default ({ mode }) => {
+  return defineConfig({
+    base: mode === 'production' ? '/trivia-explorer/' : '/',
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [postcssNested],
+      },
     },
-  },
-})
+  })
+}
