d6a8838e7a7152cc126ef7595da8dca68f2015a2
Sonia Ruiz <yune.vk@gmail.com>
2024-03-07 00:04:01 +0100

    🔧 Add Vite base config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import postcssNested from 'postcss-nested'
 
+const mode = import.meta.env.MODE
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: mode === 'production' ? '/trivia-explorer/' : '/',
   plugins: [react()],
   css: {
     postcss: {
